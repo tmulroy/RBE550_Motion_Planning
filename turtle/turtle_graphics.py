c13a20bb455bff44e24d6a169fa31c7b48e7018f
tmulroy <thomasjohnmulroy@gmail.com>
2024-01-18 12:20:51 -0500

    add hardcoded solution

--- turtle/turtle_graphics.py
+++ turtle/turtle_graphics.py
@@ -1,16 +1,5 @@
 from turtle import *
 
-
-# Diameter
-# arc length
-# turn angle
-# Forward 1/2 diameter
-# Turn Right by 120 degrees
-# Forward second leg ( determine by arc length)
-# Turn right by 120 degrees
-# Forward by diameter
-# Turn right by 120 degrees
-
 color('black', 'white')
 begin_fill()
 while True:
@@ -19,8 +8,3 @@ while True:
     forward(150)
     right(120)
     forward(100)
-    # if abs(pos()) < 1:
-    #     break
-# end_fill()
-# done()
-# print('done')
