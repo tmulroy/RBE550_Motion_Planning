83ca0cc7c7f730723dfa58d0b1c640cd31e806f8
tmulroy <thomasjohnmulroy@gmail.com>
2024-01-31 14:55:31 -0500

    add graph class to flatland

---
+++ flatland/graph.py
@@ -0,0 +1,76 @@
+# Code from geeksforgeeks.org
+# https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/?ref=lbp
+# https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/?ref=lbp
+
+from collections import defaultdict
+
+# This class represents a directed graph
+# using adjacency list representation
+
+
+class Graph:
+
+    # Constructor
+    def __init__(self):
+
+        # Default dictionary to store graph
+        self.graph = defaultdict(list)
+
+    # Function to add an edge to graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    # Function to print a BFS of graph
+    def BFS(self, s):
+
+        # Mark all the vertices as not visited
+        visited = [False] * (max(self.graph) + 1)
+
+        # Create a queue for BFS
+        queue = []
+
+        # Mark the source node as
+        # visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+
+            # Dequeue a vertex from
+            # queue and print it
+            s = queue.pop(0)
+            print(s, end=" ")
+
+            # Get all adjacent vertices of the
+            # dequeued vertex s.
+            # If an adjacent has not been visited,
+            # then mark it visited and enqueue it
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+        # A function used by DFS
+    def DFSUtil(self, v, visited):
+
+        # Mark the current node as visited
+        # and print it
+        visited.add(v)
+        print(v, end=' ')
+
+        # Recur for all the vertices
+        # adjacent to this vertex
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.DFSUtil(neighbour, visited)
+
+    # The function to do DFS traversal. It uses
+    # recursive DFSUtil()
+    def DFS(self, v):
+
+        # Create a set to store visited vertices
+        visited = set()
+
+        # Call the recursive helper function
+        # to print DFS traversal
+        self.DFSUtil(v, visited)
---
+++ flatland/motion_planner.py
@@ -0,0 +1,3 @@
+
+class MotionPlanner:
+    pass
