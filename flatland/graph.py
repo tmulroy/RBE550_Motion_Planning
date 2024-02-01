830479aa95b9b0aa70417714345e7056863be6db
tmulroy <thomasjohnmulroy@gmail.com>
2024-02-01 11:14:38 -0500

    add working bfs

--- flatland/graph.py
+++ flatland/graph.py
@@ -21,36 +21,76 @@ class Graph:
         self.graph[u].append(v)
 
     # Function to print a BFS of graph
-    def BFS(self, s):
+    # def BFS(self, s):
+    #     # TODO: start BFS from a start node (source node)
+    #     print(f'graph[s]: {self.graph[s]}')
+    #     print(f'size of graph: {self.graph}')
+    #     # Mark all the vertices as not visited
+    #     visited = [False] * (max(self.graph) + 1)
+    #
+    #     # Create a queue for BFS
+    #     queue = []
+    #
+    #     # Mark the source node as
+    #     # visited and enqueue it
+    #     queue.append(s)
+    #     # print(f's: {s}')
+    #     visited[s] = True
+    #
+    #     while queue:
+    #         # Dequeue a vertex from
+    #         # queue and print it
+    #         s = queue.pop(0)
+    #         print(s, end=" ")
+    #
+    #         # Get all adjacent vertices of the
+    #         # dequeued vertex s.
+    #         # If an adjacent has not been visited,
+    #         # then mark it visited and enqueue it
+    #         for i in self.graph[s]:
+    #             if visited[i] == False:
+    #                 queue.append(i)
+    #                 visited[i] = True
 
-        # Mark all the vertices as not visited
-        visited = [False] * (max(self.graph) + 1)
+    # def bfs(self, start, end):
+    #     # maintain a queue of paths
+    #     queue = []
+    #     # push the first path into the queue
+    #     queue.append([start])
+    #     while queue:
+    #         # get the first path from the queue
+    #         path = queue.pop(0)
+    #         # get the last node from the path
+    #         node = path[-1]
+    #         # path found
+    #         print(f'node: {node}')
+    #         if node == end:
+    #             return path
+    #         # enumerate all adjacent nodes, construct a
+    #         # new path and push it into the queue
+    #         for adjacent in self.graph.get(node, []):
+    #             new_path = list(path)
+    #             new_path.append(adjacent)
+    #             queue.append(new_path)
 
-        # Create a queue for BFS
-        queue = []
-
-        # Mark the source node as
-        # visited and enqueue it
-        queue.append(s)
-        visited[s] = True
+    def bfs(self, start, end):
+        # Source: https://stackoverflow.com/questions/8922060/how-to-trace-the-path-in-a-breadth-first-search
+        # Source: Author: @SeasonShot
+        queue = [(start, [start])]
+        visited = set()
 
         while queue:
+            vertex, path = queue.pop(0)
+            visited.add(vertex)
+            for node in self.graph[vertex]:
+                if node == end:
+                    return path + [end]
+                else:
+                    if node not in visited:
+                        visited.add(node)
+                        queue.append((node, path + [node]))
 
-            # Dequeue a vertex from
-            # queue and print it
-            s = queue.pop(0)
-            print(s, end=" ")
-
-            # Get all adjacent vertices of the
-            # dequeued vertex s.
-            # If an adjacent has not been visited,
-            # then mark it visited and enqueue it
-            for i in self.graph[s]:
-                if visited[i] == False:
-                    queue.append(i)
-                    visited[i] = True
-
-        # A function used by DFS
+    # A function used by DFS
     def DFSUtil(self, v, visited):
 
         # Mark the current node as visited
--- flatland/motion_planner.py
+++ flatland/motion_planner.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append('../')
+from flatland.graph import Graph
+# from turtle import GridWorld
 
 class MotionPlanner:
     pass
